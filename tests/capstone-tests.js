46f10eaae3465ffe2eb1f8b73473dee86c088d90
JonMoore9000 <jonmoore9000@gmail.com>
2017-02-19 20:52:00 -0600

    GET endpoint test success

---
+++ tests/capstone-tests.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+chai.use(chaiHttp);
+
+const {app, runServer, closeServer} = require('../server');
+
+// GET TEST
+  describe('GET endpoint', function() {
+
+    it('should return games api data', function() {
+      
+      let res;
+      return chai.request(app)
+        .get('/games')
+        .then(function(_res) {
+          res = _res;
+          res.should.have.status(200); 
+        });
+     });
+});
