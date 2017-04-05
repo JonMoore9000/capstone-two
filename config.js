e585e412c6cd8fd0c62568d2b775f30320e88054
JonMoore9000 <jonmoore9000@gmail.com>
2017-04-05 16:12:41 -0500

    fixing local db

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://jon:allison9@ds113650.mlab.com:13650/capstone-two-users';
+                      'mongodb://localhost/games';
 exports.PORT = process.env.PORT || 8080;
