8c1b3b9448c02fbaa68488667421131075cdd5ed
JonMoore9000 <jonmoore9000@gmail.com>
2017-04-10 16:50:37 -0500

    cleanup

--- users/models.js
+++ users/models.js
@@ -13,6 +13,25 @@ const UserSchema = mongoose.Schema({
   },
 });
 
+serSchema.methods.apiRepr = function() {
+  return {
+    username: this.username || '',
+    password: this.hashPassword ||
+  };
+}
+
+UserSchema.methods.validatePassword = function (password) {
+    return bcrypt
+        .compare(password, this.password)
+        .then(isValid => isValid);
+}
+
+UserSchema.statics.hashPassword = function (password) {
+    return bcrypt
+        .hash(password, 10)
+        .then(hash => hash);
+}
+
 const User = mongoose.model('users', UserSchema);
 
 module.exports = User;
