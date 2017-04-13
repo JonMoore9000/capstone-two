d1f3cbd8d30c19cab1edbcfc7abbd432f42c640f
JonMoore9000 <jonmoore9000@gmail.com>
2017-04-12 22:28:35 -0500

    adding delete game

--- favorites/models.js
+++ favorites/models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uuid = require('uuid');
 
 const favoriteSchema = mongoose.Schema({
   gameName: {
@@ -9,6 +10,7 @@ const favoriteSchema = mongoose.Schema({
     type: String,
     required: true,
   }
+  id: uuid.v4(),
 });
 
 const favorite = mongoose.model('favorites', favoriteSchema);
