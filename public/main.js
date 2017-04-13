ecb9aeef8f44dae1f30379f65afb91c2d103bc72
JonMoore9000 <jonmoore9000@gmail.com>
2017-04-12 23:34:43 -0500

    adding delete game

--- public/main.js
+++ public/main.js
@@ -36,7 +36,7 @@ function deleteGame() {
 	$('#js-games').on('click', '.delete', function(event) {
 		event.preventDefault();
 		console.log('delete button works')
-		id = $(this).parent().find('.game');
+		var id = $(this).parent().find('.game');
 		console.log(id);
 	$.ajax({
 		type: 'DELETE',
