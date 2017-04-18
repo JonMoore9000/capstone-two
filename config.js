5e2d6d96b9e9e8ed7dc562c302a369784219adaa
JonMoore9000 <jonmoore9000@gmail.com>
2017-04-18 15:25:10 -0500

    minor cleanup

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-						//'mongodb://localhost/games';
-						'mongodb://jon:allison9@ds113650.mlab.com:13650/capstone-two-users';
+						'mongodb://localhost/games';
 exports.PORT = process.env.PORT || 8080;
